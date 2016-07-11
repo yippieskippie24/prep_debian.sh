7e652488bef36f8b08556957964c6ba367d81d69
yippieskippie24 <yippieskippie24@gmail.com>
2016-07-11 17:14:56 -0500

    Create prep_debian.sh

---
+++ prep_debian.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+## Script written by Tyler M Johnson
+## https://github.com/yippieskippie24
+
+
+
+echo '######################################################'
+echo '##                                                  ##'
+echo '##                Prepare Debian                    ##'
+echo '##                                                  ##'
+echo '##    Setup script for new Debian server install    ##'
+echo '##                                                  ##'
+echo '######################################################'
